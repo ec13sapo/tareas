3aa8366efbfb8d67657b60ace8a930b66cf7bc5f
ec13sapo <ezequiel.saporito@gmail.com>
2023-12-10 22:50:53 -0300

    .

--- M4U4/app.js
+++ M4U4/app.js
@@ -22,6 +22,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/ingresar', function(req,res){
+  var parametro = req.body.parametro || '';
+
+  var parametro1 = 'helado';
+  var parametro2 = 'carne';
+  var parametro3 = 'agua';
+
+  var respuesta1 = "El kilogramo de helado cuesta $5000.";
+  var respuesta2 = "El kilogramo de carne cuesta $10000.";
+  var respuesta3 = "El litro de agua cuesta $1000.";
+
+  switch(parametro){
+    case(parametro1):
+      res.send(respuesta1);
+      break;
+    case(parametro2):
+    res.send(respuesta2);
+    break;
+    case(parametro3):
+      res.send(respuesta3);
+      break;
+  }
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
